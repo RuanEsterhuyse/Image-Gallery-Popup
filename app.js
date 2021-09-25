19452b72da3bf7f59cdd44b1e92f6878e5ce9ec0
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2021-09-25 19:45:56 +0200

    update

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const fullImg = document.querySelector(".full-img");
 const smallImg = document.querySelector(".gallery img");
 const modal = document.querySelector(".modal");
+
+smallImg.forEach(function(img){
+    img.addEventListener("click", function(){
+        modal.classList.add("open");
+    });
+});
